af0ec2fd29612740055e77e5f6bf01de3ca93dc0
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-23 09:57:05 +0530

    integrated model training and saving with SGD optimizer and finalized chatbot model

--- model_training/model_training.py
+++ model_training/model_training.py
@@ -63,3 +63,10 @@ model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+model.save('model/chatbot_model.keras')
+print('Done')
