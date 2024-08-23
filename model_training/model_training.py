9b6956f17edecebdd8ffdfa3d56525fba1f762f6
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-23 12:36:55 +0530

    fix data inconsistency issue in training set

--- model_training/model_training.py
+++ model_training/model_training.py
@@ -1,33 +1,34 @@
-import random
+import os
 import json
 import pickle
 import numpy as np
-import tensorflow as tf
-
 import nltk
-from nltk.stem import WordNetLemmatizer
 
-nltk.download('punkt_tab')
-nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('intents.json').read())
+intents = json.load(open('intents.json'))
 
 words = []
 classes = []
 documents = []
-ignoreLetters = ['?', '!', '.', ',']
+ignore_letters = ['?', '!', '.', ',']
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        wordList = nltk.word_tokenize(pattern)
-        words.extend(wordList)
-        documents.append((wordList, intent['tag']))
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 classes = sorted(set(classes))
@@ -36,35 +37,37 @@ pickle.dump(words, open('model/words.pkl', 'wb'))
 pickle.dump(classes, open('model/classes.pkl', 'wb'))
 
 training = []
-outputEmpty = [0] * len(classes)
+output_empty = [0] * len(classes)
 
 for document in documents:
     bag = []
-    wordPatterns = document[0]
-    wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+
     for word in words:
-        bag.append(1) if word in wordPatterns else bag.append(0)
+        bag.append(1) if word in word_patterns else bag.append(0)
 
-    outputRow = list(outputEmpty)
-    outputRow[classes.index(document[1])] = 1
-    training.append(bag + outputRow)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
 
-random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
+train_x = np.array([x for x, _ in training])
+train_y = np.array([y for _, y in training])
 
-trainX = training[:, :len(words)]
-trainY = training[:, len(words):]
+print(f'Train X shape: {train_x.shape}')
+print(f'Train Y shape: {train_y.shape}')
 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation='relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(64, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, weight_decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
 model.save('model/chatbot_model.keras')
-print('Done')
